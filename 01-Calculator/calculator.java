51e73e408d6777c66b70a8c6b522bf47bfb01059
voidstack02 <kpulkit493@gmail.com>
2026-08-22 14:41:25 +0530

    Add files via upload

--- 01-Calculator/calculator.java
+++ 01-Calculator/calculator.java
@@ -1,1 +1,37 @@
+import java.util.*;
 
+public class calculator {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("\n=======Basic Calculator=======");
+
+        System.out.println("\nEnter the first Number: ");
+        float a=sc.nextFloat();
+
+        System.out.println("\nEnter the operator(+,-,*,/,%):  ");
+        String c=sc.next();
+
+        System.out.println("\nEnter the second number: ");
+        float b=sc.nextFloat();
+
+
+        switch(c){
+            case "+" -> System.out.println("\nRESULT= "+(a+b));
+            case "-" -> System.out.println("\nRESULT= "+(a-b));
+            case "*" -> System.out.println("\nRESULT= "+(a*b));
+            case "/" -> {
+             if(b==0){
+                    System.out.println("\nNot Divisible by zero");    
+                }
+                else{
+                    System.out.println("\nRESULT= "+(a/b));
+                }
+             }           
+            case "%" -> System.out.println("\nRESULT= "+((a/b)*100));
+       
+        }
+
+    }
+    
+}
